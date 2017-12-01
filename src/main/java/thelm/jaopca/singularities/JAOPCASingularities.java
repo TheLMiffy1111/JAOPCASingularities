0b1af1ae81762e456ca517e4e8cd89e07045862d
TheLMiffy1111 <1111miffy@argentum.net>
2017-12-01 22:12:36 +0800

    2.1.0.6
    Updated to use rarity settings

--- src/main/java/thelm/jaopca/singularities/JAOPCASingularities.java
+++ src/main/java/thelm/jaopca/singularities/JAOPCASingularities.java
@@ -11,12 +11,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 		modid = JAOPCASingularities.MOD_ID,
 		name = JAOPCASingularities.NAME,
 		version = JAOPCASingularities.VERSION,
-		dependencies = "required-after:avaritia;required-before:jaopca@[1.11.2-2.1.0.34,)"
+		dependencies = "required-after:avaritia;required-before:jaopca@[1.11.2-2.1.5.62,)"
 		)
 public class JAOPCASingularities {
 	public static final String MOD_ID = "jaopcasingularities";
 	public static final String NAME = "JAOPCASingularities";
-	public static final String VERSION = "1.11.2-2.1.0.4";
+	public static final String VERSION = "1.11.2-2.1.0.6";
 	@Instance(JAOPCASingularities.MOD_ID)
 	public static JAOPCASingularities core;
 	@SidedProxy(serverSide="thelm.jaopca.singularities.CommonProxy",clientSide="thelm.jaopca.singularities.ClientProxy")
