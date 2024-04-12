bb35d8778eda822db4b01fb14ed016cbb02f36cb
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-04-13 03:21:24 +0800

    Update deps

--- changelog.md
+++ changelog.md
@@ -1,7 +1,1 @@
-Change buildscript to custom loom  
-Renamed the module to `avaritia_singularity`  
-Made the module passive  
-Add item class to Eternal Singularity automatically  
-Add logo to mod list entry  
-Remove unnecessary mod instance  
-Change asset indentation to tabs  
+Rebuild against latest JAOPCA  
--- src/main/java/thelm/jaopca/singularities/JAOPCASingularities.java
+++ src/main/java/thelm/jaopca/singularities/JAOPCASingularities.java
@@ -13,5 +13,5 @@ public class JAOPCASingularities {
 	public static final String MOD_ID = "jaopcasingularities";
 	public static final String NAME = "JAOPCASingularities";
 	public static final String VERSION = "1.12.2-0@VERSION@";
-	public static final String DEPENDENCIES = "required-after:avaritia;required-before:jaopca@[1.12.2-2.3.2,);";
+	public static final String DEPENDENCIES = "required-after:avaritia;required-before:jaopca@[1.12.2-2.3.10,);";
 }
