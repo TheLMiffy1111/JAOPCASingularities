e1cca091cc0018c2659ab6666d97d8a74f65b612
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-04-13 03:21:20 +0800

    Update deps

--- src/main/java/thelm/jaopca/singularities/JAOPCASingularities.java
+++ src/main/java/thelm/jaopca/singularities/JAOPCASingularities.java
@@ -13,5 +13,5 @@ public class JAOPCASingularities {
 	public static final String MOD_ID = "jaopcasingularities";
 	public static final String NAME = "JAOPCASingularities";
 	public static final String VERSION = "1.7.10-0@VERSION@";
-	public static final String DEPENDENCIES = "required-after:Avaritia;required-before:jaopca@[1.7.10-W.0.1,);";
+	public static final String DEPENDENCIES = "required-after:Avaritia;required-before:jaopca@[1.7.10-W.0.5,);";
 }
